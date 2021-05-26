3562d7aa9fe21e128950f3632e7dfc0b569c2d73
EwelinaStasiak <ewelina.stasiak5@gmail.com>
2021-05-26 11:06:57 +0200

    Change name class Supplier to TravelAgency

--- src/Codecool.CodecoolShop/Daos/ITravelAgencyDao.cs
+++ src/Codecool.CodecoolShop/Daos/ITravelAgencyDao.cs
@@ -2,7 +2,7 @@ using Codecool.CodecoolShop.Models;
 
 namespace Codecool.CodecoolShop.Daos
 {
-    public interface ISupplierDao : IDao<TravelAgency>
+    public interface ITravelAgencyDao : IDao<TravelAgency>
     {
     }
 }
