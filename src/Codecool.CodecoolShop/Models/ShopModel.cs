64fe96ab787f4403435923c4741c1c273fc8645a
EwelinaStasiak <ewelina.stasiak5@gmail.com>
2021-05-29 13:10:52 +0200

    changed constructor and ConfigureClassProperties for country

--- src/Codecool.CodecoolShop/Models/ShopModel.cs
+++ src/Codecool.CodecoolShop/Models/ShopModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Codecool.CodecoolShop.Controllers;
 using Codecool.CodecoolShop.Search;
 using Codecool.CodecoolShop.Services;
@@ -9,7 +10,7 @@ namespace Codecool.CodecoolShop.Models
     public class ShopModel
     {
         public List<Product> ProductsList;
-        public List<string> CountriesList { get; set; }
+        public List<Country> CountriesList { get; set; }
         public List<TravelAgency> TravelAgenciesList { get; set; }
         public List<ProductCategory> CategoriesList { get; set; }
         
@@ -17,18 +18,24 @@ namespace Codecool.CodecoolShop.Models
         public List<IFilterable> AgenciesOptions { get; private set; }
         public SelectList TravelAgencies { get; private set; }
         public int TravelAgencyId { get; set; }
+        public SelectList Countries { get; private set; }
+        public int CountryId { get; set; }
+        
         
         
         public ShopModel()
         {
         }
 
-        public ShopModel(ProductService productService, int agencyId = 0)
+        public ShopModel(ProductService productService, int agencyId = 0, int countryId = 0)
         {
             Products = productService.GetProductsForCategory(1);
             AgenciesOptions = new TravelAgency().GetSelectOptions(Products);
             TravelAgencies = new SelectList(AgenciesOptions, "Id", "Name");
             TravelAgencyId = agencyId;
+            CountriesList = productService.GetAllCountries().ToList();
+            Countries = new SelectList(CountriesList, "Id", "Name");
+            CountryId = countryId;
         }
 
         public void ConfigureClassProperties(ProductService productService, IEnumerable<Product> selectedProducts)
@@ -37,6 +44,8 @@ namespace Codecool.CodecoolShop.Models
             var allProducts = productService.GetProductsForCategory(1);
             AgenciesOptions = new TravelAgency().GetSelectOptions(allProducts);
             TravelAgencies = new SelectList(AgenciesOptions, "Id", "Name");
+            CountriesList = productService.GetAllCountries().ToList();
+            Countries = new SelectList(CountriesList, "Id", "Name");
         }
     }
 }
